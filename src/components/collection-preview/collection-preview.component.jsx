321d000f002cd5621f0310e4d973eff9fb250ba5
Rattanachai <spyneverdie@hotmail.com>
2021-05-13 03:46:46 +0700

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -11,8 +11,11 @@ const CollectionPreview = ({ title, items }) => (
       {
         items
           .filter((item, idx) => idx < 4)
-          .map(({ id, ...otherItemProps }) => (
-            <CollectionItem key={id} {...otherItemProps} />
+          // .map(({ id, ...otherItemProps }) => (
+          //   <CollectionItem key={id} {...otherItemProps} />
+          // ))
+          .map(item => (
+            <CollectionItem key={item.id} item={item} />
           ))
       }
     </div>
